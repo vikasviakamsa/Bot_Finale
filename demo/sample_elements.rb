2d6eeb0930b46a34d060b8a0aa858c654073116a
vikasviakamsa <vikasy@andrew.cmu.edu>
2017-10-17 15:55:52 -0400

    5_3

--- demo/sample_elements.rb
+++ demo/sample_elements.rb
@@ -25,7 +25,7 @@ module SampleElements
     {
       title: 'Random image',
       # Horizontal image should have 1.91:1 ratio
-      image_url: 'https://unsplash.it/750/200?random',
+      image_url: 'https://in.pinterest.com/pin/405394403945669193/',
       subtitle: "That's a first card in a carousel",
       default_action: {
         type: 'web_url',
