84bf533d1236a3cebd740f2e1108a5131fe0149c
vikasviakamsa <vikasy@andrew.cmu.edu>
2017-10-19 13:04:10 -0400

    11_1

--- bot.rb
+++ bot.rb
@@ -23,8 +23,8 @@ Rubotnik::PersistentMenu.enable
 # NOTE: QuickReplies.build should be called with a splat operator
 # if a set of quick replies is an array of arrays.
 # e.g. UI::QuickReplies.build(*replies)
-HINTS = UI::QuickReplies.build(["Where am I?", "LOCATION"],["Search New", "CAROUSEL"],
-                               ["Create a board", "QUESTIONNAIRE"])
+HINTS = UI::QuickReplies.build(["Create a board", "QUESTIONNAIRE"],["Search New", "CAROUSEL"],
+                               ["Where am I?", "LOCATION"])
 
 # Build a quick reply that prompts location from user
 LOCATION_PROMPT = UI::QuickReplies.location
